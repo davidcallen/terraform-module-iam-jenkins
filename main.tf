19c586bbf51d8445113e8832c9c0f14a8153a92a
davidcallen <david.c.allen1971@gmail.com>
2022-02-15 17:48:26 +0000

    Add Route53 DNS resolution.

--- main.tf
+++ main.tf
@@ -1,12 +1,13 @@
 # ---------------------------------------------------------------------------------------------------------------------
 # IAM Role for use by an EC2 instance of Jenkins Controller to give access to :
-#   1) EC2 instance create/start/stop/...
-#   2) Jenkins Controller get config files from S3
-#   3) Cloudwatch logging and metrics
-#   4) Check AutoScalingGroup for number of instances so can delay mounting EFS until no instances using it.
-#   5) Send SNS messages for alerting
-#   6) Send Update on instance health to the ASG
-#   7) Get secret for jenkins admin password
+#   1) Register DNS record to Route53 for our ec2 host
+#   2) Jenkins Controller manipulate EC2 for launching Jenkins Worker EC2 instance
+#   3) Jenkins Controller get config files from S3
+#   4) Cloudwatch logging and metrics
+#   5) Check AutoScalingGroup for number of instances so can delay mounting EFS until no instances using it.
+#   6) Send SNS messages for alerting
+#   7) Send Update on instance health to the ASG
+#   8) Get secret for jenkins admin password
 # ---------------------------------------------------------------------------------------------------------------------
 resource "aws_iam_role" "jenkins-controller" {
   name                 = "${var.resource_name_prefix}-jenkins-controller"
@@ -29,7 +30,37 @@ EOF
   tags                 = var.tags
 }
 
-# 1) Jenkins Controller manipulate EC2
+# 1) Register DNS record to Route53 for our ec2 host
+resource "aws_iam_policy" "route53" {
+  name        = "${var.resource_name_prefix}-nexus-route53"
+  description = "RegisterDNSwithRoute53"
+  policy      = <<EOF
+{
+  "Version": "2012-10-17",
+  "Statement": [
+    {
+      "Sid": "Route53registerDNS",
+      "Action": [
+        "route53:ChangeResourceRecordSets",
+        "route53:GetHostedZone",
+        "route53:ListResourceRecordSets"
+      ],
+      "Effect": "Allow",
+      "Resource": [
+        "arn:aws:route53:::hostedzone/${var.route53_private_zone_id}"
+      ]
+    }
+  ]
+}
+EOF
+}
+resource "aws_iam_role_policy_attachment" "route53" {
+  role       = aws_iam_role.jenkins-controller.name
+  policy_arn = aws_iam_policy.route53.arn
+}
+
+# 2) Jenkins Controller manipulate EC2 for launching Jenkins Worker EC2 instance
+# TODO : restrict this policy by Resource to jenkins-workers only - otherwise to much power !
 resource "aws_iam_policy" "jenkins-controller-ec2" {
   name        = "${var.resource_name_prefix}-jenkins-controller-ec2"
   description = "ReadWrite access to EC2 for Jenkins Controller to launch/terminate EC2 instances for Jenkins Nodes."
@@ -73,7 +104,7 @@ resource "aws_iam_role_policy_attachment" "jenkins-controller-ec2" {
   policy_arn = aws_iam_policy.jenkins-controller-ec2.arn
 }
 
-# 2) Jenkins Controller get config files from S3
+# 3) Jenkins Controller get config files from S3
 resource "aws_iam_policy" "jenkins-controller-s3" {
   name        = "${var.resource_name_prefix}-jenkins-controller-s3"
   description = "Read access to s3 bucket for Jenkins Controller config files"
@@ -99,19 +130,19 @@ resource "aws_iam_role_policy_attachment" "jenkins-controller-s3" {
   policy_arn = aws_iam_policy.jenkins-controller-s3.arn
 }
 
-# 3) Cloudwatch logging and metrics - To allow output of metrics and logs to Cloudwatch
+# 4) Cloudwatch logging and metrics - To allow output of metrics and logs to Cloudwatch
 resource "aws_iam_role_policy_attachment" "jenkins-controller-cloudwatch" {
   role       = aws_iam_role.jenkins-controller.name
   policy_arn = "arn:aws:iam::aws:policy/CloudWatchAgentServerPolicy"
 }
 
-# 4) Check AutoScalingGroup for number of instances so can delay mounting EFS until no instances using it.
+# 5) Check AutoScalingGroup for number of instances so can delay mounting EFS until no instances using it.
 resource "aws_iam_role_policy_attachment" "jenkins-controller-asg" {
   role       = aws_iam_role.jenkins-controller.name
   policy_arn = "arn:aws:iam::aws:policy/AutoScalingReadOnlyAccess"
 }
 
-# 5) Send SNS messages for alerting
+# 6) Send SNS messages for alerting
 resource "aws_iam_policy" "jenkins-controller-sns" {
   name        = "${var.resource_name_prefix}-jenkins-controller-sns"
   description = "Add ability to send SNS alert message"
@@ -136,7 +167,7 @@ resource "aws_iam_role_policy_attachment" "jenkins-controller-sns" {
   policy_arn = aws_iam_policy.jenkins-controller-sns.arn
 }
 
-# 6) Send Update on instance health to the ASG
+# 7) Send Update on instance health to the ASG
 resource "aws_iam_policy" "jenkins-controller-asg-health" {
   name        = "${var.resource_name_prefix}-jenkins-controller-asg-health"
   description = "Send Update on instance health to the ASG"
@@ -161,7 +192,7 @@ resource "aws_iam_role_policy_attachment" "jenkins-controller-asg-health" {
   policy_arn = aws_iam_policy.jenkins-controller-asg-health.arn
 }
 
-# 7) Get secret for jenkins admin password
+# 8) Get secret for jenkins admin password
 resource "aws_iam_policy" "jenkins-controller-get-secrets" {
   name        = "${var.resource_name_prefix}-jenkins-controller-get-secrets"
   description = "Get secret for jenkins admin password"
